54a780792d9d309672d13be4ecc6ed8ed0e707ed
Dariusz Okonski <darek200180@gmail.com>
2022-04-28 12:39:52 +0100

    add animation to FeedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import { PropTypes } from 'prop-types';
 import FeedbackItem from "./FeedbackItem"
 
@@ -8,16 +9,39 @@ function FeedbackList({ feedback, handleDelete }) {
 
     return (
         <div className="feedback-list">
-            {
-                feedback.map(item => 
-                    <FeedbackItem 
-                        key={item.id} 
-                        {...item} 
-                        handleDelete={handleDelete}
-                    />)
-            }
+            <AnimatePresence>
+                {
+                    feedback.map(item => 
+                        <motion.div 
+                            key={item.id} 
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}    
+                        >
+                            <FeedbackItem 
+                                key={item.id} 
+                                {...item} 
+                                handleDelete={handleDelete}
+                            />
+                        </motion.div>
+                    )
+                }
+            </AnimatePresence>
         </div>
-  )
+    )
+
+    // return (
+    //     <div className="feedback-list">
+    //         {
+    //             feedback.map(item => 
+    //                 <FeedbackItem 
+    //                     key={item.id} 
+    //                     {...item} 
+    //                     handleDelete={handleDelete}
+    //                 />)
+    //         }
+    //     </div>
+    // )
 }
 
 FeedbackList.defaultProps = {
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,6 @@
 import { PropTypes } from 'prop-types';
 
 function FeedbackStats({ feedback }) {
-    console.log(feedback)
     let average = (feedback.reduce((acc, curr) => acc + curr.rating, 0)) / feedback.length;
     average = average.toFixed(1).replace(/[.,]0$/, '');
 
