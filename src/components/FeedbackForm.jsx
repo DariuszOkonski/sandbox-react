a0a6840fdf9b79286c7c803b8c1f06f594ae8d14
Dariusz Okonski <darek200180@gmail.com>
2022-04-28 11:42:22 +0100

    add simple validation to FeedbackForm

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,30 @@
 import Card from "./shared/Card"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from "./shared/Button";
 
 function FeedbackForm() {
-    const [text, setText] = useState('')
+    const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('')
+
 
     const handleTextChange = (e) => {
         setText(e.target.value)
     }
+
+    useEffect(() => {
+        if(text === '') {
+            setBtnDisabled(true);
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setBtnDisabled(false);
+            setMessage(null)
+        }
+    }, [text])
+    
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -31,8 +48,11 @@ function FeedbackForm() {
 
                     <Button 
                         type='submit' 
+                        isDisabled={btnDisabled}
                     >Send</Button>
                 </div>
+
+                { message && <div className="message">{message}</div>}
             </form>
         </Card>
     )
